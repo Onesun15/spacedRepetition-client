8263266b0cb3c9e5ee1db207142ce86e1a55154b
Onesun15 <sunnysharma15@hotmail.com>
2018-01-06 18:42:44 -0500

    hello component

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,7 @@ import HeaderBar from './header-bar';
 import LandingPage from './landing-page';
 import Dashboard from './dashboard';
 import RegistrationPage from './registration-page';
+import Hello from './hello';
 import {refreshAuthToken} from '../actions/auth';
 
 export class App extends React.Component {
@@ -45,6 +46,7 @@ export class App extends React.Component {
                 <Route exact path="/" component={LandingPage} />
                 <Route exact path="/dashboard" component={Dashboard} />
                 <Route exact path="/register" component={RegistrationPage} />
+                <Route exact path="/hello" component={Hello} />
             </div>
         );
     }
